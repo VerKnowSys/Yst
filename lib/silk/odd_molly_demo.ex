98575f509b28cc7b91ac5b178aafe6ecbf5ebc77
Daniel (dmilith) Dettlaff <dmilith@me.com>
2016-12-03 18:02:01 +0100

    imp: Some info about odd_molly_demo.ex

--- lib/silk/odd_molly_demo.ex
+++ lib/silk/odd_molly_demo.ex
@@ -1,6 +1,11 @@
 defmodule OddMollyDemo do
+  @moduledoc "
+  More specific test site definition - OddMollyDemo
+  "
   use SilkCommon
 
+
   def url, do: "https://oddmollynew.youngskilled.com/ams"
+
 
 end
