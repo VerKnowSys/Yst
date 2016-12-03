d1f00f115110e3227b45b2353f738389f7e5f549
Daniel (dmilith) Dettlaff <dmilith@me.com>
2016-12-03 17:59:19 +0100

    imp: Screenshotting feature is already provided by go/1

--- lib/yst.ex
+++ lib/yst.ex
@@ -4,7 +4,6 @@ defmodule Yst do
   require Logger
 
   use Hound.Helpers
-  alias Hound.Helpers.Screenshot
 
 
   def main, do: main []
@@ -32,20 +31,9 @@ defmodule Yst do
     Hound.start_session
 
     OddMollyDemo.go :login
-    Logger.info "Title go :login: #{page_title}"
-    _ = Screenshot.take_screenshot "after-login.png"
-
     OddMollyDemo.go :sales
-    Logger.info "Title go :sales: #{page_title}"
-    _ = Screenshot.take_screenshot "after-sales.png"
-
     OddMollyDemo.go :customers
-    Logger.info "Title go :customers: #{page_title}"
-    _ = Screenshot.take_screenshot "after-customers.png"
-
     OddMollyDemo.go :logout
-    Logger.info "Title go :logout: #{page_title}"
-    _ = Screenshot.take_screenshot "after-logout.png"
 
     # Automatically invoked if the session owner process crashes
     Hound.end_session
