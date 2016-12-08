3336cec02894bffac79d432b2787f158908eb6bc
Daniel (dmilith) Dettlaff <dmilith@me.com>
2016-12-08 03:28:55 +0100

    imp: Use Some/None in matcher.

--- lib/scenario/matcher.ex
+++ lib/scenario/matcher.ex
@@ -10,27 +10,30 @@ defmodule Scenario.Matcher do
   @default_user_agent Hound.Browser.user_agent :chrome_desktop
   @default_session :yst_phantomjs_session
 
-  defstruct code: (Option.some [200, 201, 202]),     # -200-202 => default success codes
+  alias Option.{Some, None}
+
+
+  defstruct code: (Some.v [200, 201, 202]),     # -200-202 => default success codes
 
             # content match
-            text: [Option.none], # List of ~r// matchers expected in page text content
-            html: [Option.none], # List of ~r// matchers expected in page source code
+            text: [None.v], # List of ~r// matchers expected in page text content
+            html: [None.v], # List of ~r// matchers expected in page source code
 
-            has_id: [Option.none],
-            has_class: [Option.none],
-            has_tag: [Option.none],
+            has_id: [None.v],
+            has_class: [None.v],
+            has_tag: [None.v],
 
-            delay_before_request: (Option.some @default_delay_secs),        # delay in seconds
-            delay_after_request: (Option.some @default_delay_secs),         # delay in seconds
+            delay_before_request: (Some.v @default_delay_secs),        # delay in seconds
+            delay_after_request: (Some.v @default_delay_secs),         # delay in seconds
 
             # check element in other frame
-            in_frame: [Option.none],
+            in_frame: [None.v],
 
             # Browser session name to be used.
-            with_agent: (Option.some @default_user_agent),
+            with_agent: (Some.v @default_user_agent),
 
             # By default each scenario is executed in separate session
-            with_session: [Option.some @default_session]
+            with_session: [Some.v @default_session]
 
             # TODO: Css matchers (?)
 
