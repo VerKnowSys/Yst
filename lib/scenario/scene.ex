bf824d15756171639d6bd194cd703fbd347c46bd
Daniel (dmilith) Dettlaff <dmilith@me.com>
2016-12-10 04:15:59 +0100

    imp: Updated Scene struct API. Disabled some unused actions and checks.

--- lib/scenario/scene.ex
+++ lib/scenario/scene.ex
@@ -20,31 +20,37 @@ defmodule Scene do
             # ui_actions:
             req!: "/",                  #  /some/request/and/http-params
             click!: [],                             # Click elements
-            focus!: [],                             # Set focus on element
+            # focus!: [],                             # Set focus on element
             fill!: [],                              # fill focused field with content
             keys!: [],                         # send keystroke or key event
             js!: [],
-            dismiss!: [],                           # Dismiss dialog or other js window
+            accept!: false,                           # Accept dialog or other js window
+            dismiss!: false,                         # Dismiss dialog or other js window
             session!: false,                         # NO new session for each scene
             screenshot!: false,
             cookies_reset!: false,
 
-            browser!: @default_browser,
-            agent!: @default_user_agent,
-            wait_before!: @default_delay_secs,       # delay in seconds
-            wait_after!: @default_delay_secs,        # delay in seconds
+            # browser!: @default_browser,
+            # agent!: @default_user_agent,
+            # wait_before!: @default_delay_secs,       # delay in seconds
+            # wait_after!: @default_delay_secs,        # delay in seconds
 
             # After all sync tasks are done => perform expectations check
             title?: [],
+            title_not?: [],
             text?: [], # List of ~r// matches expected in page text content
             text_not?: [],
             src?: [], # List of ~r// matches expected in page source code
             src_not?: [],
             frame?: [],                             # scene has content in these frames
             frame_not?: [],
+            script?: [],                            # JavaScript routines that have to return true
+            script_not?: [],
 
             id?: [],
             id_not?: [],
+            css?: [],
+            css_not?: [],
             class?: [],
             class_not?: [],
             tag?: [],
