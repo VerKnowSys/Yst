0e35ce7956c713f274e9799845f681390e7d15b0
Daniel (dmilith) Dettlaff <daniel@centra.com>
2016-12-17 01:06:22 +0100

    Version bump.

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Yst.Mixfile do
   def project do
     [
       app: :yst,
-      version: "0.4.2",
+      version: "0.4.3",
       elixir: "~> 1.3",
       build_embedded: Mix.env == :prod,
       start_permanent: Mix.env == :prod,
