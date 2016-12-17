a98e14152831b3a35e99f2ad6893a49f5d682b31
Daniel (dmilith) Dettlaff <dmilith@me.com>
2016-12-17 20:00:06 +0100

    fix: Warnings about missing parentheses (when specifying additional argument)

--- test/core_test.exs
+++ test/core_test.exs
@@ -31,11 +31,11 @@ defmodule CoreTest do
     assert Some.unwrap Some.v 12345 == 12345
 
     value_wrong = [even: "now"]
-    assert 1 == value_wrong |> Some.unwrap_or_else 1
-    assert 3 == {:anything, :else} |> Some.unwrap_or_else 3
+    assert 1 == value_wrong |> (Some.unwrap_or_else 1)
+    assert 3 == {:anything, :else} |> (Some.unwrap_or_else 3)
     assert {:anything, :else} == (Some.v {:anything, :else}) |> (Some.unwrap_or_else 3)
     assert [some: "more", time: "for"] == value |> Some.unwrap
-    assert [some: "more", time: "for"] == value |> Some.unwrap_or_else :NOWAI
+    assert [some: "more", time: "for"] == value |> (Some.unwrap_or_else :NOWAI)
   end
 
 
