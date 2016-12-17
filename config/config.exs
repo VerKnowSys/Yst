084f34cbf0200e97ac52e15b50d0249e694ec0d0
Daniel (dmilith) Dettlaff <daniel@centra.com>
2016-12-17 07:46:55 +0100

    imp: Cleanup in config.exs. More roboust and single line logger output.

--- config/config.exs
+++ config/config.exs
@@ -3,67 +3,15 @@
 use Mix.Config
 
 
-config :hound, driver: "phantomjs"
+# Yst defaults:
 config :yst, scene_timeout: 360000
 config :yst, result_timeout: 30000
 
-# config :yst, callmap: [
-#     login: [
-#       rel: "/login",
-#       expected: [
-#         {
-#           ~r/SIGN IN/, [
-#             parent_of: "somemore",
-#             child_of: "container",
-#             in_frame: "frame_1",
-#             class: "some",
-#             id: "uniqu3"
-#           ]
-#         },
-#         ~r/Username/,
-#         ~r/Password/
-#       ],
-#     ],
 
-#     logout: [
-#       rel: "/sign_out",
-#       expected: [~r/SIGN IN/, ~r/Username/, ~r/Password/],
-#     ],
+# Hound defaults:
+config :hound, driver: "phantomjs"
 
-#     sales: [
-#       rel: "/retail/sales?q=status:3",
-#       expected: [~r/Total/, ~r/Order/, ~r/Customer/, ~r/Total/],
-#     ],
 
-#     customers: [
-#       rel: "/retail/customer?q=status:1",
-#       expected: [~r/Orders/, ~r/Customer/, ~r/Total/, ~r/Created/],
-#     ],
-# ]
-
-# This configuration is loaded before any dependency and is restricted
-# to this project. If another project depends on this project, this
-# file won't be loaded nor affect the parent project. For this reason,
-# if you want to provide default values for your application for
-# 3rd-party users, it should be done in your "mix.exs" file.
-
-# You can configure for your application as:
-#
-#     config :yst, key: :value
-#
-# And access this configuration in your application as:
-#
-#     Application.get_env(:yst, :key)
-#
-# Or configure a 3rd-party app:
-#
-#     config :logger, level: :info
-#
-
-# It is also possible to import configuration files, relative to this
-# directory. For example, you can emulate configuration per environment
-# by uncommenting the line below and defining dev.exs, test.exs and such.
-# Configuration from the imported file will override the ones defined
-# here (which is why it is important to import them last).
-#
-#     import_config "#{Mix.env}.exs"
+# Logger defaults:
+config :logger, level: :debug, truncate: 2048
+config :logger, :console, format: "$time|$levelpad$message\n"
