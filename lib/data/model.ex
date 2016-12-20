1596d74bcc356dd63c3265e06e8a338f53651ba7
Daniel (dmilith) Dettlaff <daniel@centra.com>
2016-12-20 15:24:29 +0100

    new: Defined Model with RecordedScenario table.

---
+++ lib/data/model.ex
@@ -0,0 +1,20 @@
+use TransactMacro
+
+
+defdatabase Model do
+  @moduledoc """
+  Basic data Model for Amnesia, Elixir 1.3.x:
+
+  RecordedScenario:
+    uuid,
+    timestamp,
+    content,
+    metadata
+
+  """
+
+  deftable RecordedScenario, [:uuid, :content, :metadata, :timestamp], type: :set do
+    @type t :: %RecordedScenario{uuid: String.t, content: String.t, metadata: String.t, timestamp: String.t}
+  end
+
+end
