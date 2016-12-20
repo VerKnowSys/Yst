a7bc28ecd8e4863852c11b72bbfad15458d1f280
Daniel (dmilith) Dettlaff <daniel@centra.com>
2016-12-20 15:26:49 +0100

    new: Added Amnesia init block for Yst.

--- lib/yst.ex
+++ lib/yst.ex
@@ -23,9 +23,19 @@ defmodule Yst do
   def environment, do: System.get_env "MIX_ENV"
 
 
+  def init_amnesia do
+    Cfg.set_default_mnesia_dir Cfg.project_dir
+    Logger.info "Yst is launching DbApi for Amnesia in dir: #{Cfg.project_dir}"
+    DbApi.init_and_start
+    DbApi.dump_mnesia "current"
+  end
+
+
   def run, do: main []
   def main, do: main []
   def main _ do
+    init_amnesia
+
     case Yst.start_link do
       {:ok, _} ->
         Logger.info "Yst-Supervisor started (env: #{environment})"
