2f4e7aea19121d0365b0a865d8d5f0789cdc1981
Daniel (dmilith) Dettlaff <daniel@centra.com>
2016-12-20 14:51:04 +0100

    new: Defined ScenarioServer

---
+++ lib/scenario/scenario_server.ex
@@ -0,0 +1,36 @@
+defmodule ScenarioServer do
+  use Maru.Router
+
+  before do
+    plug Plug.Logger
+  end
+
+  plug Plug.Parsers,
+    pass: ["*/*"],
+    json_decoder: Poison,
+    parsers: [:urlencoded, :json, :multipart]
+
+  resources do
+    get do
+      conn |> json %{ msg: "I'm scenario server :)" }
+    end
+
+    mount ScenarioServerApi
+  end
+
+
+  rescue_from [MatchError, RuntimeError], with: :custom_error
+
+  rescue_from :all do
+    conn
+    |> (put_status 500)
+    |> (json %{ msg: "Internal failure. Blame dmilith :)", error: true })
+  end
+
+  defp custom_error(conn, exception) do
+    conn
+    |> (put_status 500)
+    |> (json %{ msg: "#{exception.message}", error: true })
+  end
+
+end
