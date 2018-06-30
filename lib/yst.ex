5b4011ae9d09790fb6050c5d5437add1535f097b
Daniel (dmilith) Dettlaff <dmilith@me.com>
2018-07-01 01:19:43 +0200

    new: imp: Don't duplicate env value, use Cfg.env() instead

--- lib/yst.ex
+++ lib/yst.ex
@@ -21,17 +21,9 @@ defmodule Yst do
   end
 
 
-  def environment do
-    case System.get_env("MIX_ENV") do
-      "" -> "dev"
-      env -> env
-    end
-  end
-
-
   def init_amnesia do
     Cfg.set_default_mnesia_dir Cfg.project_dir()
-    Logger.info "Yst is launching DbApi for Amnesia in dir: #{Cfg.project_dir}"
+    Logger.info "Yst is launching DbApi for Amnesia in dir: #{Cfg.project_dir()}"
     DbApi.init_and_start()
     DbApi.dump_mnesia "current"
   end
@@ -40,13 +32,13 @@ defmodule Yst do
   def run, do: main []
   def main, do: main []
   def main _ do
-    environment = environment()
     case Yst.start_link() do
       {:ok, _} ->
-        Logger.info "Yst-Supervisor started (env: #{environment})"
+        Logger.info "Yst-Supervisor started (env: #{Cfg.env()})"
 
       {:error, {:already_started, _}} ->
-        Logger.debug "Yst-Supervisor already started (env: #{environment})"
+        Logger.info "Yst-Supervisor already started (env: #{Cfg.env()})"
+
     end
     Results.reset()
     Director.claps()
